87d19777759eda6d37542e41e9f4fbc998c0f516
linhktqd <linhktqd54@gmail.com>
2025-08-23 05:43:21 +0700

    deploy

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /**
  * @type {import('next').NextConfig}
  */
+
+const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
+  basePath: isProd ? '/blog-starter-app' : '',
   output: 'export',
   distDir: 'dist',
   images: {
